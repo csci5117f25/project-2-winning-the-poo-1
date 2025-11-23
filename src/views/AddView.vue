04253d2ea43ff5dd254b3b14e27d7787136b5d4b
wendy-mengo <wendy.w.meng@gmail.com>
2025-11-23 08:52:37 -0600

    some format

---
+++ src/views/AddView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <h1></h1>
+</template>
+
+<style scope>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+    <div class="top">
+        <h1>MEDIAQ</h1>
+        <p>Never lose your place in the queue again</p>
+    
+    </div>
+    <div class="bottom">
+
+    </div>
+</template>
+
+<style scope>
+.top{
+    background-color: gainsboro;
+    height: 50vh;
+    width: 100dvw;
+    left: 0px;
+    right: 0px;
+    align-items: center;
+}
+.bottom{
+    background-color: white;
+    height: 40vh;
+    width: 100dvw;
+    left: 0px;
+    right: 0px;
+}
+</style>
