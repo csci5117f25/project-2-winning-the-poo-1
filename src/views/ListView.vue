c514e739554e24162e9e77d53dc02585f317e281
wendy-mengo <wendy.w.meng@gmail.com>
2025-11-24 18:26:00 -0600

    list mobile edits

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -25,14 +25,16 @@ if (user.value) {
   <p>IN-PROGRESS</p>
   <div class="in-progress">
     <div v-for="item in inProgress" :key="item.name" class="card">
-      <RouterLink :to="{ name: 'media_w_id', params: { id: item.id } }">{{ item.name}}</RouterLink>
+        <RouterLink :to="{ name: 'media_w_id', params: { id: item.id } }">{{ item.name}}</RouterLink>
+        <p>{{ item.time }} hours</p>
     </div>
   </div>
 
   <p>QUEUE</p>
   <div class="queue">
     <div v-for="item in queue" :key="item.name" class="card">
-      <RouterLink :to="{ name: 'media_w_id', params: { id: item.id } }">{{ item.name}}</RouterLink>
+        <RouterLink :to="{ name: 'media_w_id', params: { id: item.id } }">{{ item.name}}</RouterLink>
+        <p>{{ item.time }} hours</p>
     </div>
   </div>
 </template>
@@ -49,11 +51,15 @@ if (user.value) {
     background: white;
     padding: 10px;
     border-radius: 10px;
-    min-width: 150px;
-    width: 5vw;
-    height: 5vw;
+    width: 10vw;
+    height: 17vh;
     margin: 1rem;
     box-shadow: 0px 8px 28px rgba(0, 0, 0, 0.2);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center; 
 }
 .card a {
     text-decoration: none; 
@@ -68,5 +74,25 @@ if (user.value) {
 }
 .queue{
     gap: 10rem;
+}
+.in-progress::-webkit-scrollbar {
+  height: 6px;
+}
+.in-progress::-webkit-scrollbar-thumb {
+  background-color: rgba(0,0,0,0.3);
+  border-radius: 3px;
+}
+@media (max-width: 600px) {
+  .card {
+    min-width: 120px;
+    max-width: 160px;
+    padding: 8px;
+  }
+  h1 {
+    font-size: 1.5rem;
+  }
+  p {
+    font-size: 0.8rem;
+  }
 }
 </style>
