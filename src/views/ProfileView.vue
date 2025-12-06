59bf07b26bd304019e0e8ec17435c26d29f7d088
Gunnar J. <GunnarJag@gmail.com>
2025-12-06 16:21:42 -0600

    completed items show up on your profile, along with the total length of media you've consumed.

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useCurrentUser, useCollection } from 'vuefire'
-import { collection } from 'firebase/firestore'
+import { collection, query, where } from 'firebase/firestore'
 import { computed } from 'vue'
 import { db } from '../firebase_conf'
 
@@ -10,12 +10,19 @@ const queueRef = collection(db, 'users', user.value.uid, 'queue');
 
 const queueItems = useCollection(queueRef);
 
+const completedQuery = query(queueRef, where("status", "==", "complete"))
+let complete = useCollection(completedQuery)
+
 const inProgressTime = computed(() => //I thought this needed curly brackets? idk man
   queueItems.value.filter(item => item.status === 'in-progress').reduce((accumulator, item) => accumulator + item.time, 0)
 )
 
 const queueTime = computed(() => //I thought this needed curly brackets? idk man
   queueItems.value.filter(item => item.status === 'queued').reduce((accumulator, item) => accumulator + item.time, 0)
+)
+
+const completedTime = computed(() => //I thought this needed curly brackets? idk man
+  queueItems.value.filter(item => item.status === 'complete').reduce((accumulator, item) => accumulator + item.time, 0)
 )
 </script>
 
@@ -34,7 +41,7 @@ const queueTime = computed(() => //I thought this needed curly brackets? idk man
           </figure>
 
           <h1 class="title">Profile</h1>
-          <h2 class="subtitle">Hi, {{ user?.displayName }}</h2>
+          <h2 class="subtitle">Hi, {{ user?.displayName }}!</h2>
 
           <hr/>
 
@@ -63,6 +70,25 @@ const queueTime = computed(() => //I thought this needed curly brackets? idk man
 
     </div>
   </section>
+  <div>
+    <!-- new: see complete items (largely reusing code from ListView)
+     TODO: make this look pretty -->
+    <div>
+      <h3>Completed Items</h3>
+      <p v-if="complete.length > 0">You've finished <strong>{{ completedTime }}</strong> minutes of content.</p>
+      <!-- could put some responsive statement here (that's more than a week!, etc.) -->
+    </div>
+    <div class="completed">
+      <div v-for="item in complete" :key="item.name" class="card">
+          <RouterLink :to="{ name: 'media_w_id', params: { id: item.id } }">{{ item.name}}</RouterLink>
+          <img v-if="item.image_url" :src="item.image_url" alt="Cover Image" class="card-img"/>
+          <p>{{ item.time }} minutes</p>
+      </div>
+      <div v-if="complete.length === 0">
+        <p>You haven't finished anything on your list? Get out there, then!</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 
