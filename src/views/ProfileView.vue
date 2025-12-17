8d36c61f37f40346b479e8b2ae8b7040d0d776f8
wendy-mengo <136666386+wendy-mengo@users.noreply.github.com>
2025-12-17 09:53:04 -0600

    total time consumed css

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -457,6 +457,10 @@ function dateCompletedAt(timestamp) {
 
 <div class="chart" v-if="selectedCategory !== 'other'">
   <StatsLineChart :data="chartData" :label="chartLabel" />
+  <h4 class="title is-4 has-text-centered">
+    Total time consumed filtered media: {{ totalDays > 0 ? totalDays + (totalDays === 1 ? ' day' : ' days') + ', ' : '' }}{{ remainderHours > 0 ? remainderHours + (remainderHours === 1 ? ' hour' : ' hours') + ', ' : '' }}{{ remainderMinutes > 0 ? remainderMinutes + (remainderMinutes === 1 ? ' minute' : ' minutes') : '0 minutes' }}
+    <p v-if="totalFilteredPages > 0">{{ totalFilteredPages.toLocaleString() }} pages read</p>
+  </h4>
 </div>
 
 <template v-if="selectedCategory === 'other'">
@@ -488,7 +492,7 @@ function dateCompletedAt(timestamp) {
 .chart{
   max-width: 50vw;
   height: 50vh;
-  margin: 0 auto;
+  margin: 0 auto 3rem auto; 
 }
 @media (max-width: 768px) {
   .chart {
